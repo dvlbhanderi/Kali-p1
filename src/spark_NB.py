303aaf9eb02b74769acbd8cf2b4d8bcd3e860d3d
Adurden <durden4th@gmail.com>
2019-02-07 09:44:10 -0500

    restructured to run more efficiently using pipeline

--- src/spark_NB.py
+++ src/spark_NB.py
@@ -1,71 +1,54 @@
-import sys
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.ml.classification import NaiveBayes
-from pyspark.ml.feature import CountVectorizer
+from pyspark.ml.feature import CountVectorizer, HashingTF, Tokenizer
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.feature import StringIndexer
 
 sc = SparkContext.getOrCreate()
 
 spark = SparkSession(sc)
 
+xfile = open("X_small_train.txt")
+X_train = xfile.read().splitlines()
+yfile = open("y_small_train.txt")
+y_train = yfile.read().splitlines()
 
-X_train = sc.textFile(sys.argv[1])
-y_train = sc.textFile(sys.argv[2])
+byte_data_directory = 'file:/home/durden/Desktop/Practicum/Kali-p1/data/'
 
-"""
-X_train = sc.parallelize(['DvdM5Zpx96qKuN3cAt1y','5QpgRV2cqU9wvjBist1a','2F6ZfVCQRi3vrwcj4zxL','1KjZ4An78sOytkzgRL0E'])
-y_train = sc.parallelize([6,3,1,7])
-"""
-print(y_train.count())
-print('short files read')
+X_filenames = list(map(lambda x: byte_data_directory+x+'.bytes', X_train))
 
-# read directory of byte files
-byte_data_directory = sys.argv[3]
+label_map = sc.broadcast(dict(zip(X_filenames, y_train)))
 
-# form full filename from directory and names and loas into list
-X_filenames = X_train.map(lambda x: byte_data_directory+x+'.bytes')
-X_filenames = X_filenames.collect()
-label_map = dict(zip(X_filenames, [int(i) for i in y_train.collect()))
-label_map = sc.broadcast(label_map)
-print('dat read')
-# load pairRDD of text to preproc and train on
 dat_train = sc.wholeTextFiles(",".join(X_filenames))
-print(dat_train.count())
-dat_train = dat_train.map(lambda x: (x[0], x[1], label_map.value[x[0]]))
-y_train.unpersist()
+dat_train = dat_train.map(lambda x: (x[0], x[1], float(label_map.value[x[0]])))
+dat_train = dat_train.toDF(['filname', 'text', 'category']).repartition(12)
 
-dat_train = dat_train.map(lambda x: (x[0], x[1].split(), x[2]))
-dat_train = dat_train.map(lambda x: (x[0], list(filter(lambda y: len(y) == 2, x[1])), x[2]))
-dat_df = dat_train.toDF(['filename', 'words', 'label'])
-dat_train.unpersist()
-
-print(dat_df.rdd.getNumPartitions())
-dat_df = dat_df.repartition(32)
-print('repartitioned', dat_df.rdd.getNumPartitions())
-
-
-cv = CountVectorizer(inputCol="words", outputCol="features")
-dat_df = cv.fit(dat_df).transform(dat_df)
-print(dat_df.show())
-
+label_stringIdx = StringIndexer(inputCol="category", outputCol="label")
+tokenizer = Tokenizer(inputCol="text", outputCol="words")
+hashingTF = HashingTF(numFeatures=256, inputCol=tokenizer.getOutputCol(),
+                      outputCol="features")
 nb = NaiveBayes(smoothing=1)
-model = nb.fit(dat_df)
-"""
-del dat_df
+pipeline = Pipeline(stages=[tokenizer, hashingTF, label_stringIdx, nb])
 
+model = pipeline.fit(dat_train)
 
-# read in the testing data using same process as above
-X_test = sc.textFile(sys.argv[4])
-X_test_filenames = X_test.map(lambda x: byte_data_directory+x+'.bytes')
-X_test_filenames = X_test_filenames.collect()
-dat_test = sc.wholeTextFiles(",".join(X_test_filenames))
-"""
+xfile = open("X_small_test.txt")
+X_test = xfile.read().splitlines()
+yfile = open("y_small_test.txt")
+y_test = yfile.read().splitlines()
 
-"""
+X_filenames_test = list(map(lambda x: byte_data_directory+x+'.bytes', X_test))
 
-dat_test = dat_test.mapValues(lambda x: x.split())
-dat_test = dat_test.mapValues(lambda x: list(filter(lambda y: len(y) == 2, x)))
-test_df = dat_test.toDF(['filename', 'words'])
-preds = model.transform(test_df)
-preds.show()
-"""
+label_map_test = sc.broadcast(dict(zip(X_filenames_test, y_test)))
+
+dat_test = sc.wholeTextFiles(",".join(X_filenames_test))
+dat_test = dat_test.map(lambda x: (x[0], x[1], float(label_map_test.value[x[0]])))
+dat_test = dat_test.toDF(['filname', 'text', 'category']).repartition(12)
+
+pred = model.transform(dat_test)
+pred.select("filname", "label", "prediction").show()
+
+evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+print(evaluator.evaluate(pred))
